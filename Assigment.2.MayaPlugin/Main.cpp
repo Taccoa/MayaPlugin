a89570d47a7c0158d4f92ff18b00d7440d73b4bd
Taccoa <maml14@student.bth.se>
2016-10-06 14:42:48 +0200

    2.9

--- Assigment.2.MayaPlugin/Main.cpp
+++ Assigment.2.MayaPlugin/Main.cpp
@@ -49,8 +49,6 @@ EXPORT MStatus initializePlugin(MObject obj)
 
 	MStatus r = MS::kSuccess;
 
-
-
 	NACId = MDGMessage::addNodeAddedCallback(onNodeCreated, "dependNode", NULL, &r);
 
 	if(r==MS::kFailure)
